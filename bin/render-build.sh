efd8bf2f46531902a2ca4f69281fe2cd12baf4b8
Trần Minh Trí <tmtzminhtri@gmail.com>
2023-09-18 16:53:18 +0700

    update deploy

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,10 +6,10 @@ bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 
-bundle exec rake db:environment:set RAILS_ENV=production
+# bundle exec rake db:environment:set RAILS_ENV=production
 # bundle exec puma -t 5:5 -p ${PORT:-3000} -e ${RACK_ENV:-development}
-bundle exec rake db:reset RAILS_ENV=production
+# bundle exec rake db:reset RAILS_ENV=production DISABLE_DATABASE_ENVIRONMENT_CHECK=1 
 bundle exec rake db:migrate
-bundle exec rake db:seed
-bundle exec rake import_data:locations
-bundle exec rake import_data:borrowers
+# bundle exec rake db:seed
+# bundle exec rake import_data:locations
+# bundle exec rake import_data:borrowers
